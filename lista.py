cb70352e879dd195a13b11943f66a6f12a87a647
Manuel Iván San Martín Castillo <ivansmc00@gmail.com>
2023-11-23 19:41:17 +0100

    Práctica completa

--- lista.py
+++ lista.py
@@ -9,13 +9,17 @@ def estaEnLista(valor, lista):
         return True
     return False
 
-num = int(input("Introduce un número comprendido entre 1 y 20: "))
-lista = [6, 14, 11, 3, 2, 1, 15, 19]
+try:
+    num = int(input("Introduce un número comprendido entre 1 y 20: "))
+    lista = [6, 14, 11, 3, 2, 1, 15, 19]
 
-if (estaEnRango(num, 1, 20)):
-    if (estaEnLista(num, lista)):
-        print("El número " + str(num) + " está en la lista")
+    if (estaEnRango(num, 1, 20)):
+        if (estaEnLista(num, lista)):
+            print("El número " + str(num) + " está en la lista")
+        else:
+            print("El número " + str(num) + " no está en la lista")
     else:
-        print("El número " + str(num) + " no está en la lista")
-else:
-    print("El número " + str(num) + " no está comprendido entre 1 y 20")
+        print("El número " + str(num) + " no está comprendido entre 1 y 20")
+
+except ValueError:
+    print("ERROR: La cadena introducida no es un número")
